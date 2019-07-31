2aeb898c86d86cf9eba430fe2cd1630ddaec3de8
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-07-31 09:41:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,21 +10,22 @@
 	<center>
     	<h1>WELCOME TO REYNER'S PROFILE!</h1>
    		<h3>Reyner Paul B. Bering's Biography</h3>
-    	<p><em></em></p>
+    	<p><em>Knowledge comes with power!</em></p>
     </center>
   	</header>
 	<body>
 		<main>
+		<center>
 			<article>
-				<p>
-					
-				</p>
+				<p><h2>Education</h2></p>
+				<p><h4>SHS: USJ-R</h4></p>
+				<p><h4>JHS: SACS</h4></p>
+				<p><h4>GS: SACS</h4></p>
 			</article>
 			<aside>
-				<p>
-				
-				</p>
+				<p></p>
 			</aside>
+		</center>
 		</main>
 	<footer>
   		<p>By: Reyner Paul B. Bering</p>
