870714e1a7f590e508ad47c782c87d7fec6105ea
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-07-31 09:34:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,38 @@
-<!DOCTYPE html>
+<!doctypehtml>
+<body bgcolor="orange">
 <html>
-<head>
-	<title>Profile Page</title>
-</head>
-<body>
-1. Create a free Heroku account and GitHub account.<br>
+	<head>
+		<title>
+				Personal Profile of Reyner
+		</title>
+	</head>
+	<header>
+    	<h1>WELCOME TO REYNER'S PROFILE!</h1>
+   		<h3>Reyner Paul B. Berings	</h3>
+    	<p>Some additional information here</p>
+  	</header>
+	<body>
+		<main>
+			<article>
+				<p>
+					
+				</p>
+			</article>
+			<aside>
+				<p>
+				
+				</p>
+			</aside>
+		</main>
+	<footer>
+  		<p>Posted by: Hege Refsnes</p>
+  		<p>Contact information: <a href="https://www.facebook.com/reynerpaulbering">https://www.facebook.com/reynerpaulbering.com</a>.</p>
+  
+	</footer>
 
-2. In Heroku, create an application with name starting with your last name followed by your first name initial (e.g. CUIZONL). If your app name is CUIZONL, your app will be accessible as https://cuizonl.herokuapp.com.<br>
-
-3. Beautify and apply CSS to your HTML Workshop 01.<br>
-4. Upload the files to GitHub.<br>
-
-5. Deploy your files from GitHub to Heroku.<br>
-
-6. Paste the content of your files here and the URL to your heroku app.<br>
-
-</body>
+		
+		
+		
+	
+	</body>
 </html>
