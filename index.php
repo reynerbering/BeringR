e1b19627e7a91e6371675327d57e3ad9d3aeca26
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-07-31 09:24:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 	<title>Profile Page</title>
 </head>
 <body>
-1. Create a free Heroku account and GitHub account.
+1. Create a free Heroku account and GitHub account.<br>
 
-2. In Heroku, create an application with name starting with your last name followed by your first name initial (e.g. CUIZONL). If your app name is CUIZONL, your app will be accessible as https://cuizonl.herokuapp.com.
+2. In Heroku, create an application with name starting with your last name followed by your first name initial (e.g. CUIZONL). If your app name is CUIZONL, your app will be accessible as https://cuizonl.herokuapp.com.<br>
 
-3. Beautify and apply CSS to your HTML Workshop 01.
-4. Upload the files to GitHub.
+3. Beautify and apply CSS to your HTML Workshop 01.<br>
+4. Upload the files to GitHub.<br>
 
-5. Deploy your files from GitHub to Heroku.
+5. Deploy your files from GitHub to Heroku.<br>
 
-6. Paste the content of your files here and the URL to your heroku app.
+6. Paste the content of your files here and the URL to your heroku app.<br>
 
 </body>
 </html>
